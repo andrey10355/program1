7d03415ae8ec66fb8490032c15def88bd1d4adac
unknown <guru10355@mail.ru>
2023-07-10 23:40:00 +0300

    addtwo

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("input number: ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
