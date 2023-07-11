19d8140c2542008989be7138a113bb242600a01d
unknown <guru10355@mail.ru>
2023-07-11 19:01:13 +0300

    1

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер от 1 до 7");
+string number = Console.Readline();
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else
+{
+    ConsoleWriteLine("Ошибка");
+}
+}
