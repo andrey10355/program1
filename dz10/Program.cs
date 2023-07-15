c214b8e4d94e7f6ca5fa8c91030db38a438125f2
unknown <guru10355@mail.ru>
2023-07-15 16:27:02 +0300

    D.ZADACHA 10

---
+++ dz10/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход трёх значное число и на выходе показывает вторую цифру этого числа
+// 456-> 5
+// 782-> 2
+// 918-> 8
+
+int SecondDigit(int number1)
+{
+    return number1 / 10 % 10;
+}
+void PrintSecondDigit(int number2)
+{
+    Console.WriteLine("the secong digit of your number - " + number2 +".");
+}
+Console.WriteLine("enter a three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number <100 || number > 999)
+{
+    Console.WriteLine("Its not a three digit number. Start agan.");   
+}
+else
+{
+    int secondDigit = SecondDigit(number);
+    PrintSecondDigit(secondDigit);
+}
