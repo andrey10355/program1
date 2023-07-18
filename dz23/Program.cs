aa184e64f620165377d5abfced57ee3bc3e6e86b
unknown <guru10355@mail.ru>
2023-07-18 19:57:28 +0300

    add commit

---
+++ dz23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1
+// до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int GetNumber(string message) // пишем передаваемое сообщение
+{
+    Console.Write(message + " ");
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void Square(int num) // печатаем последовательного квадраты числа от 1 до N
+{
+int count = 1;
+while (count <= num)
+{
+   if (count == num)
+    Console.Write(count * count * count);
+else
+    Console.Write(count * count * count + " ");
+    count++;
+}
+}
+
+
+
+int n = GetNumber("Input number: "); // Запрашиваем переменную n у пользователя через фунцию GetNumber
+Square(n);  //Запускаем функцию печати квадратов
