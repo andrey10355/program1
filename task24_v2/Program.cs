cf081703bdbdc4621b8782a4ea08efca729818c1
unknown <guru10355@mail.ru>
2023-07-22 21:05:23 +0300

    add zadacha 24 variant 2

---
+++ task24_v2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Вариант с методом
+// Задача 24. Напишите программу, которая на вход принимает число (A) и выдает сумму чисел
+//от 1 до A.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void GetSumNumber(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+    sum += i;
+    }
+Console.WriteLine($"Sum of numbers from 1 to {A} -> {sum}");
+}
+int A = GetNumber("Enter first number: ");
+GetSumNumber(A);
+
+int B = GetNumber("Enter second number: ");
+GetSumNumber(B);
+
+int C = GetNumber("Enter third number: ");
+GetSumNumber(C);
