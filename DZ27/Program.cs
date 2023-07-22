a0ecd864527c22ccc36ac4f21cbce96f8000a10c
unknown <guru10355@mail.ru>
2023-07-22 21:42:20 +0300

    add dz27

---
+++ DZ27/Program.cs
@@ -0,0 +1,27 @@
+﻿//Вариант 1.
+//Задача 27. Напишите программу, которая принимает на вход число и выдает
+// сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine($"\n");
+Console.Write("Enter number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber (int numberA)
+{
+    int counter = Convert.ToString(numberA).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = numberA - numberA % 10;
+        result = result + (numberA - advance);
+        numberA = numberA / 10;
+    }
+    return result;
+}
+int sum = SumNumber(numberA);
+Console.WriteLine("the sum of the digits in a number:  " + sum);
