c80003b2199045690c9b82d4ba0bc501996487ad
unknown <guru10355@mail.ru>
2023-07-23 21:37:33 +0300

    add dz25

---
+++ dz25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит в степень
+// число A в натуральную степень B.
+// 3, 5 -> 243 (3 в степени 5)
+// 2, 4 -> 16
+// Вариант 1.
+
+Console.WriteLine($"\nRaising a number A to a natural power B");
+
+int Exponentiation(int numberA, int numberB)
+{
+    int result =1;
+    for(int i=1; i <= numberB; i++)
+    {
+        result =result * numberA;
+    }
+    return result;
+}
+Console.Write("Enter number A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int expon = Exponentiation(numberA, numberB);
+Console.WriteLine("Answer: " + expon);
+
