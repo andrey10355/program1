c6314ca4118a1dd09fe12b7ed0e8c7ee080308b2
unknown <guru10355@mail.ru>
2023-07-27 08:28:44 +0300

    add void

---
+++ task28/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 28. Напишите программу, которая принимает на вход число N и выдаёт 
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Promt(string message) // создаём метод для печати
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+ 
+void Factorial (int b)
+{
+int a = 1;
+for (int i = 1; i <= b; i++)
+a = a * i;
+Console.WriteLine($"Произведение чисел от 1 до {b} = {a}");
+} 
+
+int number = Promt("Введите число: ");
+Factorial(number);
+
+//int a = 1;
+//for (int i = 1; i <= number; i++)
+//a = a * i;
+//Console.WriteLine($"Произведение чисел от 1 до {b} = {a}");
