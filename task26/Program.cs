11513ab9ea0b02ce95e0bd875fb16b1585b29e6a
unknown <guru10355@mail.ru>
2023-07-27 07:34:29 +0300

    add zadacha26

---
+++ task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Promt(string message) // создаём метод для печати
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+int num = Promt("Enter number1: ");
+int count = 0;
+for (count = 0; num > 0; count++)
+{
+    num = num / 10;
+}
+Console.WriteLine(count);
