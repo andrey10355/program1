8ad151a2ceebd5b0da158872e3ce0970b18679c2
unknown <guru10355@mail.ru>
2023-07-29 18:27:19 +0300

    add task22

---
+++ task22/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 22.
+// Напишите программу, которая принимает на вход число (N) и выдаёт
+// таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25
+
+int number = Prompt("Enter number: ");
+int sum = 1;
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+while (sum <= number)
+{
+    Console.WriteLine($"{sum*sum}");
+    sum++;
+}
