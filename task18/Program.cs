7d1fb763c1c0e26b593848d9250a9da06a09d0af
unknown <guru10355@mail.ru>
2023-07-29 18:05:43 +0300

    add task18

---
+++ task18/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 18.
+// Напишите программу, которая по заданому номеру четверти, показвает диапазон возможных координат
+// точек в этой четверти (X and Y).
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+string GetResult( int number)
+{
+    string res = String.Empty;
+    if (number < 1 || number > 4)
+    {
+        res = "Значение не соответсвтует условию";
+    }
+    else
+    {
+        if (number == 1)
+        {
+            res = "x от 0 до + бесконечности, y от 0 до + бесконечности";
+        }
+        else if (number == 2)
+        {
+            res = "x от 0 до - бесконечности, y от 0 до + бесконечности";
+        }
+        else if (number == 3)
+        {
+            res = "x от 0 до - бескончности, y от 0 до - бесконечности";
+        }
+        else if (number == 4)
+        {
+            res = "x от 0 до + бесконечности, y от 0 до + бесконечности";
+        }
+    }
+    return res;
+}
+
+int sqr = Prompt("Введите номер четверти: ");
+Console.WriteLine(GetResult(sqr));
