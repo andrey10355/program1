c8c974fd70e3610d5145b6a6c00fc4321b96a0b0
unknown <guru10355@mail.ru>
2023-08-03 20:37:19 +0300

    dz64

---
+++ dz64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Домашняя работа.
+// Задача 64.
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке  
+// от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+
+int n = InputInt("Enter a positive number: "); //Введите положительное число
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Entered a nonpositive number"); //Ввели не положительное число
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n ==m)
+    return n;
+    else
+    Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
